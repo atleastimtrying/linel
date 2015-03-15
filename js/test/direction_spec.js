e75eee11ad3c549cf6704b8d7af20d3c841626ea
Anders Fisher <atleastimtrying@gmail.com>
2015-03-15 21:54:26 +0000

    refsctor of direction spec

--- js/test/direction_spec.js
+++ js/test/direction_spec.js
@@ -1,5 +1,6 @@
 describe('direction', function(){
   var stub, events, direction, direction_spy;
+
   beforeEach(function(){
     stub  = { direction_chosen: function(){} };
     events = new window.linel.Events();
@@ -10,49 +11,41 @@ describe('direction', function(){
 
   it('maintains a direction of 1', function(){
 
-    var state = {
-      direction: 1
-    };
+    var old_state = { direction: 1 };
+    var new_state = { direction: 1 };
 
     events.pub('input_changed', 1);
 
-    events.pub('game_continues', state);
-    expect(direction_spy).toHaveBeenCalledWith(state);
+    events.pub('game_continues', old_state);
+    expect(direction_spy).toHaveBeenCalledWith(new_state);
   });
 
   it('maintains a direction of -1', function(){
 
-    var state = {
-      direction: -1
-    };
+    var old_state = { direction: -1 };
+    var new_state = { direction: -1 };
 
     events.pub('input_changed', -1);
 
-    events.pub('game_continues', state);
-    expect(direction_spy).toHaveBeenCalledWith(state);
+    events.pub('game_continues', old_state);
+    expect(direction_spy).toHaveBeenCalledWith(new_state);
   });
 
   it('ignores a direction of 0', function(){
 
-    var state = {
-      direction: 1
-    };
+    var old_state = { direction: 1 };
+    var new_state = { direction: 1 };
 
     events.pub('input_changed', 0);
 
-    events.pub('game_continues', state);
-    expect(direction_spy).toHaveBeenCalledWith(state);
+    events.pub('game_continues', old_state);
+    expect(direction_spy).toHaveBeenCalledWith(new_state);
   });
 
   it('assigns a direction of -1', function(){
 
-    var old_state = {
-      direction: 1
-    };
-
-    var new_state = {
-      direction: -1
-    };
+    var old_state = { direction: 1 };
+    var new_state = { direction: -1 };
 
     events.pub('input_changed', -1);
 
@@ -62,13 +55,8 @@ describe('direction', function(){
 
   it('assigns a direction of 1', function(){
 
-    var old_state = {
-      direction: -1
-    };
-
-    var new_state = {
-      direction: 1
-    };
+    var old_state = { direction: -1 };
+    var new_state = { direction: 1 };
 
     events.pub('input_changed', 1);
 
