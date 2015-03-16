ba3be339777e224c9bd945c27d00299f57190af1
Anders Fisher <atleastimtrying@gmail.com>
2015-03-16 23:09:58 +0000

    position is now calculated based on velocity and previous position

--- js/src/position.js
+++ js/src/position.js
@@ -1,6 +1,10 @@
 window.linel.Position = function(events){
   var calculate_position = function(old_state){
-    events.pub('position_calculated');
+    var new_position = old_state.position + old_state.velocity;
+    events.pub('position_calculated', {
+      position: new_position,
+      velocity: old_state.velocity
+    });
   };
 
   events.sub('environment_applied', calculate_position);
--- js/test/position_spec.js
+++ js/test/position_spec.js
@@ -9,9 +9,63 @@ describe("position", function(){
     events.sub('position_calculated', stub.position_calculated);
   });
 
-  it('calculates_position after environment_applied', function(){
-    var old_state = {};
+  it('calculates_position if position negative and velocity negative', function(){
+    var old_state = {
+      position: -1,
+      velocity: -3
+    };
+
+    var new_state = {
+      position: -4,
+      velocity: -3
+    };
+
     events.pub('environment_applied', old_state);
-    expect(position_spy).toHaveBeenCalled();
+    expect(position_spy).toHaveBeenCalledWith(new_state);
+  });
+
+  it('calculates_position if position negative and velocity positive', function(){
+    var old_state = {
+      position: -1,
+      velocity: 5
+    };
+
+    var new_state = {
+      position: 4,
+      velocity: 5
+    };
+
+    events.pub('environment_applied', old_state);
+    expect(position_spy).toHaveBeenCalledWith(new_state);
+  });
+
+  it('calculates_position if position positive and velocity positive', function(){
+    var old_state = {
+      position: 56,
+      velocity: 5
+    };
+
+    var new_state = {
+      position: 61,
+      velocity: 5
+    };
+
+    events.pub('environment_applied', old_state);
+    expect(position_spy).toHaveBeenCalledWith(new_state);
+  });
+
+  it('calculates_position if position positive and velocity negative', function(){
+    var old_state = {
+      position: 1,
+      velocity: -3
+    };
+
+    var new_state = {
+      position: -2,
+      velocity: -3
+    };
+
+    events.pub('environment_applied', old_state);
+    expect(position_spy).toHaveBeenCalledWith(new_state);
   });
 });
