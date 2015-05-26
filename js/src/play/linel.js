afdc251c25afcacff9a1dbd0180e8561375290fb
Anders Fisher <atleastimtrying@gmail.com>
2015-05-26 11:09:05 +0100

    foo?

---
+++ js/src/play/linel.js
@@ -0,0 +1,16 @@
+var Linel = React.createClass({displayName: "Linel",
+  calculateLinelStyle: function(linel){
+    return {
+      strokeDasharray: linel.length + "px 100000000px",
+      strokeDashoffset: -linel.position + 'px'
+    };
+  },
+
+  render: function(){
+    var linel = this.props.linel
+    var path = this.props.path
+    return(
+      React.createElement("path", {className: "linel", style: this.calculateLinelStyle(linel), d: path})
+    );
+  }
+});
